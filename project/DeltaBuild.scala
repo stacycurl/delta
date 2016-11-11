21c12534345b655b596a6e729a04a63339365adc
Stacy Curl <stacy.curl@gmail.com>
2016-11-11 20:04:47 +0000

    Upgraded to 2.12.0

--- project/DeltaBuild.scala
+++ project/DeltaBuild.scala
@@ -13,30 +13,30 @@ object DeltaBuild extends Build {
   lazy val core = (project in file("core")
     settings(commonSettings: _*)
     settings(Publishing.settings: _*)
-    settings(libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.3")
+    settings(libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.6")
   )
 
   lazy val cats = (project configure dependantProject("cats")
-    settings(libraryDependencies += "org.typelevel" %% "cats" % "0.4.1")
-    settings addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.7.1")
+    settings(libraryDependencies += "org.typelevel" %% "cats" % "0.8.1")
+    settings addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.3")
   )
 
   lazy val scalaz = (project configure dependantProject("scalaz")
-    settings(libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.2.2")
-    settings addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.7.1")
+    settings(libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.3.0-M6")
+    settings addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.3")
   )
 
   lazy val generic = (project configure dependantProject("generic")
-    settings(libraryDependencies += "com.chuusai" %% "shapeless" % "2.1.0")
+    settings(libraryDependencies += "com.chuusai" %% "shapeless" % "2.3.2")
   )
 
   lazy val argonaut = (project configure dependantProject("argonaut")
-    settings(libraryDependencies += "io.argonaut" %% "argonaut" % "6.2-M2")
+    settings(libraryDependencies += "io.argonaut" %% "argonaut" % "6.2-RC1")
   )
 
   lazy val matchers = (project configure dependantProject("matchers")
     dependsOn argonaut % "compile -> compile; test -> test"
-    settings(libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4")
+    settings(libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1")
   )
 
   lazy val runAll = TaskKey[Unit]("run-all")
@@ -55,10 +55,14 @@ object DeltaBuild extends Build {
 
   private def commonSettings = Seq(
     organization := "com.github.stacycurl",
-    scalaVersion := "2.11.7",
+    scalaVersion := "2.12.0",
     maxErrors := 1,
     parallelExecution in Test := true,
     scalacOptions := Seq("-feature", "-Xfatal-warnings", "-deprecation", "-unchecked"),
-    libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+    libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test",
+    initialize := {
+      val _ = initialize.value
+      require(sys.props("java.specification.version") == "1.8", "Java 8 is required for this project.")
+    }
   )
 }
